c466a0babd1368c3ab2c541b04df9d2d78c5d2ea
Daniel Watkins <daniel@daniel-watkins.co.uk>
2014-07-18 19:41:53 +0100

    Refactor tests to use gocheck.

--- pr_test.go
+++ pr_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/coopernurse/gorp"
+	. "gopkg.in/check.v1"
 )
 
 func createTestDatabase() *gorp.DbMap {
@@ -19,20 +20,39 @@ func createTestDatabase() *gorp.DbMap {
 	return dbmap
 }
 
-func TestAddElection(t *testing.T) {
-	dbmap := createTestDatabase()
+func Test(t *testing.T) { TestingT(t) }
 
+type ElectionSuite struct {
+	dbmap *gorp.DbMap
+}
+
+var _ = Suite(&ElectionSuite{})
+
+func (s *ElectionSuite) SetUpSuite(c *C) {
+	s.dbmap = createTestDatabase()
+}
+
+func (s *ElectionSuite) SetUpTest(c *C) {
+	err := s.dbmap.TruncateTables()
+	if err != nil {
+		c.Error(err)
+	}
+}
+
+func (s *ElectionSuite) TearDownSuite(c *C) {
+	s.dbmap.Db.Close()
+}
+
+func (s *ElectionSuite) TestAddElectionCreatesOneElection(c *C) {
 	w := httptest.NewRecorder()
 	r, err := http.NewRequest("POST", "http://test.example.com/elections", nil)
 	checkErr(err, "Request creation failed")
 
-	testApp := CreateApplication(dbmap)
+	testApp := CreateApplication(s.dbmap)
 	testApp.router.ServeHTTP(w, r)
 
-	count, err := dbmap.SelectInt("select count(*) from elections")
+	count, err := s.dbmap.SelectInt("select count(*) from elections")
 	checkErr(err, "Getting count failed")
 
-	if count != 1 {
-		t.Error("Expected 1 election, found ", count)
-	}
+	c.Assert(count, Equals, int64(1))
 }
