914af8cb532bfad32b1bbf9b81678d7cfe959b9d
Daniel Watkins <daniel@daniel-watkins.co.uk>
2014-07-18 17:39:26 +0100

    Get string fetching of elections working.

--- pr.go
+++ pr.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/stephens2424/muxchain"
@@ -16,9 +18,21 @@ func AddElection(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func GetElection(w http.ResponseWriter, r *http.Request) {
+	id_string := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(id_string)
+	if err == strconv.ErrSyntax {
+		http.Error(w, "Invalid ID", 400)
+		return
+	}
+	election := electionDatabase.Get(id)
+	io.WriteString(w, election.String())
+}
+
 func init() {
 	router := mux.NewRouter()
 	router.HandleFunc("/elections", AddElection).Methods("POST")
+	router.HandleFunc("/elections/{id}", GetElection).Methods("GET")
 	muxchain.Chain("/", muxchainutil.DefaultLog, router)
 }
 
